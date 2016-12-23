9cfbae7a678e8de30c3b22ec972a7b8a6e979d7d
root <root@localhost.localdomain>
2016-12-22 22:26:28 -0500

    test cache line

---
+++ cpu_cache/cache_line_size.c
@@ -0,0 +1,33 @@
+#include<time.h>
+#include<stdio.h>
+ 
+long timediff(clock_t t1, clock_t t2) {
+    long elapsed;
+    elapsed = ((double)t2 - t1) / CLOCKS_PER_SEC * 1000;
+    return elapsed;
+}
+ 
+int main(int argc, char *argv[])
+{
+ 
+    int array_size=atoi(argv[1]);
+    int repeat_times = 1000000000;
+    long array[array_size];
+    int i ;
+    for(i=0; i<array_size; i++){
+        array[i] = 0;
+    }
+    int j=0;
+    int k=0;
+    int c=0;
+    clock_t start=clock();
+    while(j++<repeat_times){
+        if(k==array_size){
+            k=0;
+        }
+        c = array[k++];
+    }
+    clock_t end =clock();
+    printf("%lu\n", timediff(start,end));
+    return 0;
+}
